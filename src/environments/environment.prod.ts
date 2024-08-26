dacc739fea042564b53ad75dcc01b0eeaefacc11
Mohammad Khusnul Khuluq <59632952+khulqu15@users.noreply.github.com>
2024-08-27 05:35:30 +0900

    add prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyASTOg8Jc-vPdhsOUzEnqX5_C_0JEnhZmo",
+    authDomain: "aitoma-40747.firebaseapp.com",
+    databaseURL: "https://aitoma-40747-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "aitoma-40747",
+    storageBucket: "aitoma-40747.appspot.com",
+    messagingSenderId: "750771282383",
+    appId: "1:750771282383:web:9034a320bc626957e272ba",
+    measurementId: "G-6BSB4715B6"
+  }
 };
